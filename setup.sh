222f93737fb3e2cdfadc6583f849bb3484d55015
Danakane <li.3b@hotmail.fr>
2018-06-17 23:05:54 +0200

    setup.sh: symlink creation in /sbin

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-chmod go-rwx $DIR
+chmod go-rwx ${DIR}
+
+APPSCRIPT="nuurihyon.py"
+APPNAME="nuurihyon"
+SRCPATH=${DIR}/${APPSCRIPT}
+DSTPATH="/sbin"/${APPNAME}
+ln -sf ${SRCPATH} ${DSTPATH}
+
